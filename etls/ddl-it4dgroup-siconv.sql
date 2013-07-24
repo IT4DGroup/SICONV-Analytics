6ead077987af65718a1ae3796c8111100a465313
Caio Moreno de Souza <caio@it4biz.com.br>
2013-07-24 18:11:27 -0300

    SICONV v1.0 ETLs - Data Mart Convenios

--- etls/ddl-it4dgroup-siconv.sql
+++ etls/ddl-it4dgroup-siconv.sql
@@ -1,3 +1,5 @@
+
+
 CREATE TABLE dw.dim_estado
 (
   sk_estado BIGSERIAL
@@ -173,5 +175,94 @@ CREATE TABLE dw.stg_convenios_sincov
 , valor_contrapartida BIGINT
 , data_ultima_liberacao TIMESTAMP
 , valor_ultima_liberacao BIGINT
+)
+;
+
+CREATE TABLE dw.fato_convenios_siconv
+(
+  sk_concedente BIGINT
+, sk_convenente BIGINT
+, sk_dt_fim_vigencia DOUBLE PRECISION
+, sk_dt_inicio_vigencia DOUBLE PRECISION
+, sk_dt_publicacao DOUBLE PRECISION
+, sk_dt_ultima_liberacao DOUBLE PRECISION
+, sk_estado BIGINT
+, sk_geografia BIGINT
+, sk_municipio BIGINT
+, sk_orgao_superior BIGINT
+, sk_situacao BIGINT
+, dd_num_convenio BIGINT
+, dd_num_original TEXT
+, dd_obj_convenio TEXT
+, m_valor_contrapartida BIGINT
+, m_valor_convenio BIGINT
+, m_valor_liberado BIGINT
+, m_valor_ult_liberacao BIGINT
+, m_tempo_contrato_dias DOUBLE PRECISION
+)
+;
+
+CREATE TABLE dw.dim_tempo
+(
+  sk_tempo DOUBLE PRECISION
+, "data" TIMESTAMP
+, data_curta TEXT
+, data_media TEXT
+, data_comprida TEXT
+, data_completa TEXT
+, dia_do_ano TEXT
+, nome_dia TEXT
+, dia_abreviado TEXT
+, mes_abreviado TEXT
+, nome_mes TEXT
+, nome_trimestre TEXT
+, ano_trimestre TEXT
+, ano_mes_abreviado TEXT
+, e_ultimo_dia_do_mes TEXT
+, e_primeiro_dia_do_mes TEXT
+, ano2 TEXT
+, ano4 TEXT
+, dia_do_mes TEXT
+, semana_do_ano TEXT
+, semana_do_mes TEXT
+, mes_numero TEXT
+, trimestre_numero TEXT
+, ano_mes_numero TEXT
+, e_ultimo_dia_da_semana TEXT
+, e_primeiro_dia_da_semana TEXT
+, data_numerica TEXT
+)
+;
+
+CREATE INDEX idx_dim_tempo_lookup
+ ON dw.dim_tempo
+( 
+  sk_tempo
+, "data"
+, data_curta
+, data_media
+, data_comprida
+, data_completa
+, dia_do_ano
+, nome_dia
+, dia_abreviado
+, mes_abreviado
+, nome_mes
+, nome_trimestre
+, ano_trimestre
+, ano_mes_abreviado
+, e_ultimo_dia_do_mes
+, e_primeiro_dia_do_mes
+, ano2
+, ano4
+, dia_do_mes
+, semana_do_ano
+, semana_do_mes
+, mes_numero
+, trimestre_numero
+, ano_mes_numero
+, e_ultimo_dia_da_semana
+, e_primeiro_dia_da_semana
+, data_numerica
 )
 ;
